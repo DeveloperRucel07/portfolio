dfd2ef19e8f2d297b72ef2e1d274f709d8ae6bc7
DeveloperRucel07 <tsafackrucel@gmail.com>
2025-11-17 20:17:49 +0100

    add responice in legacy and policy / add box-shadow to ibuttons and links

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Footer } from "../../components/footer/footer";
 import { Button } from "../../components/button/button";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [Footer, Button],
+  imports: [Footer, Button, RouterLink],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
--- src/app/pages/privacy-policy/privacy-policy.ts
+++ src/app/pages/privacy-policy/privacy-policy.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Footer } from "../../components/footer/footer";
+import { Button } from "../../components/button/button";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [Footer],
+  imports: [Footer, Button, RouterLink],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
