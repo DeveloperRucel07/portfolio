01ba9d3443565dacd93d3e203daa0dde1c6bcf4e
DeveloperRucel07 <tsafackrucel@gmail.com>
2025-11-17 17:46:27 +0100

    add responsive in legal notice

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Footer } from "../../components/footer/footer";
+import { Button } from "../../components/button/button";
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [Footer],
+  imports: [Footer, Button],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
