62bbd66a3e4fa48ac8e428e1cdfd57942286c9b5
DeveloperRucel07 <tsafackrucel@gmail.com>
2025-11-30 22:49:42 +0100

    responsive for tablet

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, ViewChild} from '@angular/core';
+import { Component} from '@angular/core';
 import { Landing } from "../../components/landing/landing";
 import { AboutMe } from "../../components/about-me/about-me";
 import { Skills } from "../../components/skills/skills";
@@ -15,18 +15,6 @@ import { Footer } from "../../components/footer/footer";
   styleUrl: './home.scss',
 })
 export class Home {
-
-   @ViewChild('mainContent') scrollable!: ElementRef<HTMLDivElement>;
-
   constructor() {}
-
-  @HostListener('window:wheel', ['$event'])
-  onWheel(event: WheelEvent) {
-    if (!this.scrollable?.nativeElement) return;
-    const deltaY = event.deltaY;
-    const step = 7;
-    this.scrollable.nativeElement.scrollLeft += deltaY*step;
-  }
-  
 
 }
