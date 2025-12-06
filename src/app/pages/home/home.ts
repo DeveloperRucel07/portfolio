851ce9c4b89d6269adf3570bb2ac65e98e310fb1
DeveloperRucel07 <tsafackrucel@gmail.com>
2025-12-06 21:51:45 +0100

    implement horizontal Scroll

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit, HostListener } from '@angular/core';
 import { Landing } from "../../components/landing/landing";
 import { AboutMe } from "../../components/about-me/about-me";
 import { Skills } from "../../components/skills/skills";
@@ -14,7 +14,44 @@ import { Footer } from "../../components/footer/footer";
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
-  constructor() {}
+export class Home implements OnInit, AfterViewInit {
+  @ViewChild('mainContent') mainContent!: ElementRef<HTMLDivElement>;
+  private isLargeScreen = false;
 
+  ngOnInit() {
+    this.checkScreenSize();
+    window.addEventListener('resize', () => this.checkScreenSize());
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      if (this.mainContent?.nativeElement) {
+        this.mainContent.nativeElement.scrollLeft = 0;
+      }
+    }, 100);
+  }
+
+  private checkScreenSize() {
+    this.isLargeScreen = window.innerWidth > 1500;
+  }
+
+  @HostListener('wheel', ['$event'])
+  onMouseWheel(event: WheelEvent) {
+    if (!this.isLargeScreen) return;
+    if (!this.mainContent) return;
+    event.preventDefault();
+    const container = this.mainContent.nativeElement;
+    const scrollStep = container.clientWidth;
+    if (event.deltaY > 0) {
+      container.scrollLeft += scrollStep;
+    } else {
+      container.scrollLeft -= scrollStep;
+    }
+    if (container.scrollLeft < 0) container.scrollLeft = 0;
+    if (container.scrollLeft > container.scrollWidth - container.clientWidth) {
+      container.scrollLeft = container.scrollWidth - container.clientWidth;
+    }
+  }
+  
 }
+
