1f96f14409c348201448317cf20a53755f238447
Rucel Tsafack <tsafackrucel@gmail.com>
2026-03-21 23:31:21 +0100

    fixed deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
+source ~/.bashrc
+
 git add .
 git commit -m "$@"
 git push
 
 ng build
 
-git ftp init 
-
-
 git ftp push \
   --user $FTP_USER \
   --passwd $FTP_PASSWORD \
   --syncroot dist/portfolio/browser \
-  $FTP_HOST
+  $FTP_HOST_PORTFOLIO
 
